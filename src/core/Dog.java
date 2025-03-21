f8ce703b2ce9e9911549fd8216536bc7aefd07d4
Amfrias <amfrias@uninorte.edu.co>
2025-03-21 16:00:50 -0500

    Feat: Added Constructor Dog

--- src/core/Dog.java
+++ src/core/Dog.java
@@ -14,5 +14,13 @@ public class Dog {
     private double height;
     private double weight;
     private String race;
+
+    public Dog(String name, int age, double height, double weight, String race) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.race = race;
+    }
     
 }
