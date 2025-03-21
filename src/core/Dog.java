4dea6185fafd7f08fcbf42e6400e3e48a7ac24e5
Amfrias <amfrias@uninorte.edu.co>
2025-03-21 15:54:40 -0500

    Feat: Added Class Dog

---
+++ src/core/Dog.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author amfrias
+ */
+public class Dog {
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+    private String race;
+    
+}
