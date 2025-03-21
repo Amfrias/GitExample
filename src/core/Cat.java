c4623396cf4a001f91b74be7ca00cf58b9bc81dd
Amfrias <amfrias@uninorte.edu.co>
2025-03-21 15:59:01 -0500

    Feat: Added Class Cat

---
+++ src/core/Cat.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author amfrias
+ */
+public class Cat {
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+    private String race;
+
+    public Cat(String name, int age, double height, double weight, String race) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.race = race;
+    }
+    
+}
