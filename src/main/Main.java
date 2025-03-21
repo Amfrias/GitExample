6cb027fbe69b7b01e45dc97025fd84e7d7d04ac4
Amfrias <amfrias@uninorte.edu.co>
2025-03-21 15:37:58 -0500

    Feat: Added Person Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main;
+
+import core.Person;
 
 /**
  *
@@ -11,7 +14,8 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        Person person = new Person(1548845899L, "Juan Perez", 23);
+        System.out.println(person);
     }
     
 }
